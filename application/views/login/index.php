235fbdef47f0ae9760d12521450e57b5d26ad744
luongtopp <tranducluong1642002@gmail.com>
2022-10-30 04:00:56 +0700

    luongtopp

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -1,6 +1,6 @@
 <div class="container">
     <h3>Login Admin Page</h3>
-<form action="<?php echo base url('login-user') ?>" method="post">
+<!-- <form action="<?php echo base url('login-user') ?>" method="post"> -->
   <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
     <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
